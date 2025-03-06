cb03d5057693b8b4d8d290015fca6b1bdb158a25
genn125 <genn125@gmail.com>
2025-03-06 12:08:26 +0300

    django Начало

--- shop/models.py
+++ shop/models.py
@@ -2,16 +2,20 @@ from django.utils import timezone
 
 from django.db import models
 
+# Создание модели "категории курсов обучения" в каждой записи которой есть два поля
 class Category(models.Model):
-    title = models.CharField(max_length=255)
-    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=255) # значение в виде строки с максимальной длинной строки 255
+    created_at = models.DateTimeField(default=timezone.now) # дата автоматически генерируется с текущей датой
 
+# Создание модели "курсы обучения в составе категории"
 class Curse(models.Model):
     title = models.CharField(max_length=255)
-    price = models.FloatField()
-    students_qty = models.IntegerField()
-    reviews_qty = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    price = models.FloatField() # цена = число с плавающей точкой
+    students_qty = models.IntegerField() # количество студентов = целое число
+    reviews_qty = models.IntegerField() # количество отзывов = целое число
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # категория = ключ из другой таблицы (Категория,
+                                                                      # при удалении которой удаляются все курсы).
     created_at = models.DateTimeField(default=timezone.now)
 
 
