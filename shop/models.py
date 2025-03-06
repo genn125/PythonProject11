43e3a47f44f3ec9bba04aa321db5303fb42afe30
genn125 <genn125@gmail.com>
2025-03-06 11:35:08 +0300

    django Начало

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
+from django.utils import timezone
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+
+
+
+
+
+
+
+
+
