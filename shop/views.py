9ebc09ecbcfe31d383eb3150be23bd4c88164e51
genn125 <genn125@gmail.com>
2025-03-20 12:31:33 +0300

    django Начало

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 # Create your views here.
 
-'''функция вида, принимает запрос от клиента 
+'''функция вида, назвали index потому что принимает запрос от клиента 
 (отвечает за то, что возвращается при обращении пользователя 
 на главную страницу приложения shop)'''
 
-# 1)  функция вида
+# 1) функция вида
 def index(request):
     return HttpResponse('Hello from the Shop app')    #
