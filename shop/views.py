27583f0ab30fcc41cc27128d54e5319210bada32
genn125 <genn125@gmail.com>
2025-03-22 12:08:47 +0300

    django Начало

--- shop/views.py
+++ shop/views.py
@@ -1,40 +1,29 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
-
 # 23) Изменяем отображение нашей страницы на сайте
 # from . import models
 from .models import Course
-
 '''функция вида, назвали index потому что принимает запрос от клиента 
 (отвечает за то, что возвращается при обращении пользователя 
-на главную страницу приложения shop)'''
-'''
+на главную страницу приложения shop)
 # 1) функция вида
-#
-
-#
 # 1a) после функцию index необходимо привязать к определенному маршруту в рамках приложения shop
 # для этого в папке приложения создаем файл urls.py (в нем будут маршруты только для этого приложения
 #def index(request):
    return HttpResponse('Hello from the Shop app')
-
 # 23a)
 # def index(request):
-#     curses = Curse.objects.all()
-#     return HttpResponse(curses)
-
-# 25) Используем файл шаблона html, созданного на 24)
+#     curses = Course.objects.all()
+#     return HttpResponse(courses)
+# 25) Используем файл шаблона html, созданного на 24)  
 '''
-
 
 def index(request):
    courses = Course.objects.all()
    return render(request, 'courses.html', {'courses':courses})  # возвращаем результат вызова функции render
 # request запрос от клиента, который получает функция index, courses.html - название шаблона
-
 # 26) добавляем 3-й аргумент в render (передаём последовательность curses в функцию render)
 # и меняем шаблон, вставляя эту последовательность в шаблон в виде кода python
-
 def single_course(request, course_id):  # 28a) связь с urls.py
    course = Course.object.get(pk=course_id)  # 28b) ищем 1 курс по ид курса
    return render(request, 'single_course.html', {'course':course})
